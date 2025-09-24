387b80280abcfdd2dc73238b651a7ed20d48e387
dAnt3 <cdantequera@gmail.com>
2025-09-24 20:26:30 -0300

    ProyectoGoog775

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+  
+  return (
+    <>
+    <div className="globos-flotadores">
+        <div className="globo globo-1"></div>
+        <div className="globo globo-2"></div>
+        <div className="globo globo-3"></div>
+    </div>
+      <Header />
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Aside-left/Aside-left.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Aside-left.css'
+
+function Aside() {
+  return (
+    <div>Aside-left</div>
+  )
+}
+
+export default Aside
